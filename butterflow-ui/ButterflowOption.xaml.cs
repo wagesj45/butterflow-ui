89b138e37bfd4ef250bd81c758a1fb764d369945
Jordan Wages <jordan@jordanwages.com>
2018-06-22 22:10:37 -0400

    Initial commit.
    Basic UI structure. Nothing functional.

---
+++ butterflow-ui/ButterflowOption.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace butterflow_ui
+{
+    /// <summary>
+    /// Interaction logic for ButterflowOption.xaml
+    /// </summary>
+    public partial class ButterflowOption : UserControl
+    {
+        #region Properties
+
+        public String LabelValue
+        {
+            get
+            {
+                return (String)GetValue(LabelValueProperty);
+            }
+            set
+            {
+                SetValue(LabelValueProperty, value);
+            }
+        }
+
+        #endregion
+
+        #region Dependency Properties
+
+        public static DependencyProperty LabelValueProperty = DependencyProperty.Register("LabelProperty", typeof(String), typeof(ButterflowOption));
+
+        #endregion
+
+        public ButterflowOption()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ butterflow-ui/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace butterflow_ui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFilePicker_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Supported Video Files|*.mp4;*.mkv";
+
+            var result = ofd.ShowDialog(this);
+            if (result.HasValue && result.Value)
+            {
+                txtFileName.Text = ofd.FileName;
+                mediaPreview.Source = new Uri(ofd.FileName);
+            }
+        }
+    }
+}
